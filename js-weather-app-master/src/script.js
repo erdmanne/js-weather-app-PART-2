25a58ec081783ecb803cbdc85cd0d1be23777213
Eva Erdmann <erdmanne@mediamarktsaturn.com>
2020-01-30 11:27:18 +0100

    add ESlint and minimize my script files made a separate file for src and for dist
    ESLInt  /*eslint-env jquery*/ was the key
    
    add some more css stylings to get h1 to "bounce" :)

--- js-weather-app-master/src/script.js
+++ js-weather-app-master/src/script.js
@@ -1,9 +1,11 @@
+
+ /*eslint-env jquery*/
 //stuff for user input (city) ---Dear Emmanuel - it does not work - where is my mistake??? :(
 
-//var api = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=0595215f568d90110031d71a7b16a440";
-//var citytype = "London";
-//var apiKey = "&APPID=0595215f568d90110031d71a7b16a440";
-//var units = "&units=metric";
+//var api = 'http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=0595215f568d90110031d71a7b16a440';
+//var citytype = 'London';
+//var apiKey = '&APPID=0595215f568d90110031d71a7b16a440';
+//var units = '&units=metric';
 //var url = api + citytype + apiKey + units;
 
 
@@ -11,25 +13,25 @@
 
 
 //get the API from openweathermap & with console.log checking if data is loaded
-jQuery.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=0595215f568d90110031d71a7b16a440",
+jQuery.getJSON('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=0595215f568d90110031d71a7b16a440',
 function(data){
   console.log(data);
 
 
 //now get the default icons which openweathermap is giving me displayed - created therefore in the indey.html the class for it
 
-  var icon = "http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png";
+  var icon = 'http://openweathermap.org/img/w/'+ data.weather[0].icon + '.png';
   var temp = Math.floor(data.main.temp); //get the temperatur displayed like the icon & hint: Math.floor ist für die Rundung
   var weather = data.weather[0].main;// get the weather with words displayed
   var city  = data.name;
   var detailWeather = data.weather[0].description; //get descpription for the details in the modal
 
 
-  jQuery(".icon").attr('src', icon);
-  jQuery(".temp").append(temp);
-  jQuery(".weather").append(weather);
-  jQuery(".city").append(city);
-  jQuery(".detailWeather").append(detailWeather);
+  jQuery('.icon').attr('src', icon);
+  jQuery('.temp').append(temp);
+  jQuery('.weather').append(weather);
+  jQuery('.city').append(city);
+  jQuery('.detailWeather').append(detailWeather);
 
 
 
@@ -40,19 +42,19 @@ function(data){
 
 
 //****modal stuff****
-//***most important statement to get jquery "started"
+//***most important statement to get jquery 'started'
 $(document).ready(function(){
 //  alert($);   ***for checking if jquery is installed and up and running
 //just for fun to give the page more life
-$("#divBtn").hide().delay(1000).show(3000);
+$('#divBtn').hide().delay(1000).show(3000);
 
 
-//show the modal when clicking on the button "more details"
-//$("#modalButton").on("click", function(){
-//  $("#modal").show(); //show the modal when clicking
-//hide the modal again when clicking "x"
-//$("#closeButton").on("click", function(){
-//  $("#modal").hide();
+//show the modal when clicking on the button 'more details'
+//$('#modalButton').on('click', function(){
+//  $('#modal').show(); //show the modal when clicking
+//hide the modal again when clicking 'x'
+//$('#closeButton').on('click', function(){
+//  $('#modal').hide();
 
 
 //new modal with bootstrap
